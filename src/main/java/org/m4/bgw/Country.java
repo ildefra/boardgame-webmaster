04486231e4da30c898d5555aef4216dc27d110b5
ildefra <defranceschi.java@gmail.com>
2015-09-24 13:30:04 +0200

    database reverse engineer --schema boardgames

---
+++ src/main/java/org/m4/bgw/Country.java
@@ -0,0 +1,12 @@
+package org.m4.bgw;
+import org.springframework.roo.addon.dbre.annotations.RooDbManaged;
+import org.springframework.roo.addon.javabean.annotations.RooJavaBean;
+import org.springframework.roo.addon.javabean.annotations.RooToString;
+import org.springframework.roo.addon.jpa.annotations.activerecord.RooJpaActiveRecord;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord(versionField = "", table = "country")
+@RooDbManaged(automaticallyDelete = true)
+public class Country {
+}
