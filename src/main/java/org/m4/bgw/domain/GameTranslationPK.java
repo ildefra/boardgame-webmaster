c4f2c51c3ff9fc135da35fb576ab1cc740eb9eb2
ildefra <defranceschi.java@gmail.com>
2015-10-13 15:05:04 +0200

    starting from scratch with a script
    trying to make it work from an empty project to a full-fledged web
    application: currently I am blocked on the "perform tests" line

--- src/main/java/org/m4/bgw/domain/GameTranslationPK.java
+++
@@ -1,8 +0,0 @@
-package org.m4.bgw.domain;
-import org.springframework.roo.addon.jpa.annotations.identifier.RooIdentifier;
-import org.springframework.roo.addon.javabean.annotations.RooEquals;
-
-@RooIdentifier(dbManaged = true)
-@RooEquals
-public final class GameTranslationPK {
-}
--- src/test/java/org/m4/bgw/domain/AvgGameLengthDataOnDemand.java
+++
@@ -1,6 +0,0 @@
-package org.m4.bgw.domain;
-import org.springframework.roo.addon.dod.annotations.RooDataOnDemand;
-
-@RooDataOnDemand(entity = AvgGameLength.class)
-public class AvgGameLengthDataOnDemand {
-}
