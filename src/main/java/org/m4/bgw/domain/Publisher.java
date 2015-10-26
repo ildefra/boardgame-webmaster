86900d779c8cc73df9aa14e5f0d0c26cc0136771
ildefra <defranceschi.java@gmail.com>
2015-10-26 12:02:52 +0100

    #8 publisher name in all views (update does not work)

--- src/main/java/org/m4/bgw/domain/Publisher.java
+++ src/main/java/org/m4/bgw/domain/Publisher.java
@@ -1,26 +1,25 @@
 package org.m4.bgw.domain;
+
 import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+
 
 @Entity
 @Table(name = "publisher")
 public class Publisher {
-
-	public String toString() {
-        return new ReflectionToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).setExcludeFieldNames("boardgames").toString();
-    }
-
-	@OneToMany(mappedBy = "publisher")
-    private Set<Boardgame> boardgames;
+	
+    @Id
+    @Column(name = "name", length = 50)
+    private String name;
 
 	@Column(name = "website", length = 255, unique = true)
     @NotNull
@@ -30,12 +29,17 @@ public class Publisher {
     @NotNull
     private byte[] logo;
 
-	public Set<Boardgame> getBoardgames() {
-        return boardgames;
+	
+    @OneToMany(mappedBy = "publisher")
+    private Set<Boardgame> boardgames;
+
+
+    public String getName() {
+        return this.name;
     }
 
-	public void setBoardgames(Set<Boardgame> boardgames) {
-        this.boardgames = boardgames;
+    public void setName(String id) {
+        this.name = id;
     }
 
 	public String getWebsite() {
@@ -54,16 +58,19 @@ public class Publisher {
         this.logo = logo;
     }
 
-	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "name", length = 50)
-    private String name;
 
-	public String getName() {
-        return this.name;
+    public Set<Boardgame> getBoardgames() {
+        return boardgames;
     }
 
-	public void setName(String id) {
-        this.name = id;
+    public void setBoardgames(Set<Boardgame> boardgames) {
+        this.boardgames = boardgames;
+    }
+	
+	
+    @Override
+    public String toString() {
+        return new ReflectionToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .setExcludeFieldNames("boardgames").toString();
     }
 }
