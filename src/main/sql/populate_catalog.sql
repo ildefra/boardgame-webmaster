acd569cdd15b3055a694d3eb68cc746bfed6bd57
ildefra <defranceschi.java@gmail.com>
2015-10-26 17:29:39 +0100

    #22 populate game designers

--- src/main/sql/populate_catalog.sql
+++ src/main/sql/populate_catalog.sql
@@ -7,3 +7,17 @@ VALUES
 	('Amigo', 'https://www.amigo-spiele.de/'),
 	('Level 99 Games', 'http://www.lvl99games.com/'),
 	('Libellud', 'http://www.libellud.com/');
+
+INSERT INTO `game_designer` (`first_name`, `surname`)
+VALUES
+	('William', 'Attia'),
+	('Arnaud', 'Demaegd'),
+	('Michael', 'Tummelhofer'),
+	('Michael', 'Menzel'),
+	('Wolfgang', 'Kramer'),
+	('Franz', 'Vohwinkel'),
+	('Brad', 'Talton, Jr.'),
+	('Fabio', 'Fontes'),
+	('Regis', 'Bonnessee'),
+	('Xavier', 'Durin'),
+	('Michael', 'Kiesling');
