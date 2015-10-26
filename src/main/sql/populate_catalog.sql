b22786b1af1b4ccb12b9ef21f263c2d4ce8a9ec4
ildefra <defranceschi.java@gmail.com>
2015-10-26 17:19:05 +0100

    #22 populate publishers

---
+++ src/main/sql/populate_catalog.sql
@@ -0,0 +1,9 @@
+use `boardgames`;
+
+INSERT INTO `publisher` (`name`, `website`)
+VALUES
+	('Ystari Games', 'http://www.ystari.com'),
+	('Hans Im Gluck', 'http://www.hans-im-glueck.de/'),
+	('Amigo', 'https://www.amigo-spiele.de/'),
+	('Level 99 Games', 'http://www.lvl99games.com/'),
+	('Libellud', 'http://www.libellud.com/');
