88729993700d9a9fd17fa650e2f90d9876f90b24
ildefra <defranceschi.java@gmail.com>
2015-10-26 14:35:50 +0100

    #8 handling of new column name (still not viewed)

--- src/main/java/org/m4/bgw/domain/UserLevel.java
+++ src/main/java/org/m4/bgw/domain/UserLevel.java
@@ -1,5 +1,7 @@
 package org.m4.bgw.domain;
+
 import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,23 +10,23 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
+
 
 @Entity
 @Table(name = "user_level")
 public class UserLevel {
 
-	public String toString() {
-        return new ReflectionToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).setExcludeFieldNames("levelTranslations", "players").toString();
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "level_id")
+    private Short levelId;
 
-	@OneToMany(mappedBy = "levelId")
-    private Set<LevelTranslation> levelTranslations;
-
-	@OneToMany(mappedBy = "levelId")
-    private Set<Player> players;
-
+    @Column(name = "name", length = 20)
+    private String name;
+    
 	@Column(name = "can_play")
     @NotNull
     private boolean canPlay;
@@ -37,22 +39,30 @@ public class UserLevel {
     @NotNull
     private boolean canDowngrade;
 
-	public Set<LevelTranslation> getLevelTranslations() {
-        return levelTranslations;
+	
+    @OneToMany(mappedBy = "levelId")
+    private Set<LevelTranslation> levelTranslations;
+
+    @OneToMany(mappedBy = "levelId")
+    private Set<Player> players;
+
+
+    public Short getLevelId() {
+        return this.levelId;
     }
 
-	public void setLevelTranslations(Set<LevelTranslation> levelTranslations) {
-        this.levelTranslations = levelTranslations;
+    public void setLevelId(Short id) {
+        this.levelId = id;
     }
 
-	public Set<Player> getPlayers() {
-        return players;
+    public String getName() {
+        return this.name;
     }
 
-	public void setPlayers(Set<Player> players) {
-        this.players = players;
+    public void setName(String name) {
+        this.name = name;
     }
-
+    
 	public boolean isCanPlay() {
         return canPlay;
     }
@@ -77,16 +87,27 @@ public class UserLevel {
         this.canDowngrade = canDowngrade;
     }
 
-	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "level_id")
-    private Short levelId;
 
-	public Short getLevelId() {
-        return this.levelId;
+    public Set<LevelTranslation> getLevelTranslations() {
+        return levelTranslations;
     }
 
-	public void setLevelId(Short id) {
-        this.levelId = id;
+    public void setLevelTranslations(Set<LevelTranslation> levelTranslations) {
+        this.levelTranslations = levelTranslations;
+    }
+
+    public Set<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Set<Player> players) {
+        this.players = players;
+    }
+	
+	
+    @Override
+    public String toString() {
+        return new ReflectionToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .setExcludeFieldNames("levelTranslations", "players").toString();
     }
 }
