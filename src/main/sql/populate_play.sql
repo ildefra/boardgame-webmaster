397081a480d775fc3a5b819fc196912842be85c9
ildefra <defranceschi.java@gmail.com>
2015-10-27 08:06:07 +0100

    #23 populate players + template for the other inserts

---
+++ src/main/sql/populate_play.sql
@@ -0,0 +1,75 @@
+use `boardgames`;
+
+
+INSERT INTO `player`
+(
+	`username`, `email`, `password`, `registration_dtm`, `level_id`,
+	`sex`, `birth_date`, `country`, `motto`
+)
+VALUES
+	(
+	'alonso', 'alonso@gmail.com', 'ferrari', '2012-06-27 05:25:00', 2,
+	1, '1981-07-29', 'es', 'hola todos'
+	),
+	(
+	'lagrippe', 'lagrippe@gmail.com', 'lamerestbleu', '2015-10-26 23:23:23', 2,
+	2, '1987-04-20', 'fr', 'heureux au jeu, malheureux en amour'
+	),
+	(
+	'rudegirl', null, 'gaelic', '2014-02-17 17:02:00', 4,
+	2, '1986-02-08', 'ie', 'stay hungry stay angry'
+	),
+	(
+	'jabbas', 'jabbas@gmail.com', 'maigret', '2011-03-16 16:03:00', 5,
+	0, '1983-08-07', 'ua', 'burning the candle at both ends'
+	),
+	(
+	'sdrea', 'sdrevolution@hotmail.it', 'magamago', '2012-12-21 21:12:12', 2,
+	2, '1987-03-08', 'it', 'the winner is... me!'
+	),
+	(
+	'prode', 'prodens@gmail.com', 'vivalavita', '2015-08-13 13:08:00', 1,
+	0, '1979-05-02', 'it', 'la machina de la sorte decide i destini'
+	),
+	(
+	'ivankaramazov', null, 'fedorovich', '2013-07-15 15:07:00', 2,
+	1, '1985-10-14', 'ru', 'popof is alive'
+	),
+	(
+	'jasper', 'jasper@gmail.com', 'ilovetrain', '2010-09-12 12:09:00', 3,
+	1, '2001-06-16', 'us', 'play nice or leave'
+	),
+	(
+	'pzue', 'zueg@live.com', 'noodleandsoya', '2010-01-01 00:00:00', 3,
+	1, '1972-11-13', 'cn', 'if the rope is long, the kite will soar'
+	),
+	(
+	'omgponies', 'furrylover@aol.com', 'eviltwilight', '2015-03-22 03:23:00', 2,
+	1, '1980-12-31', 'us', 'keep calm and wag your tail'
+	);
+
+
+INSERT INTO `achieved`
+(
+	`username`, `boardgame_id`, `achievement_id`, `on_date`
+)
+VALUES
+	('alonso', 1, 1, '2013-07-28');
+
+
+INSERT INTO `game_table`
+(
+	`boardgame_id`, `time_limit_id`, `rated`, `video_recorded`,
+	`created_dtm`, `game_started_dtm`, `game_ended_dtm`
+)
+VALUES
+	(
+	1, 5, 1, 0,
+	'2012-06-27 17:56:00', '2012-06-27 17:59:00', '2012-06-27 18:05:00'
+	);
+
+
+INSERT INTO `played` (`username`, `game_table_id`, `score`)
+VALUES
+	('pzue', 1, 1),
+	('omgponies', 1, 0);
