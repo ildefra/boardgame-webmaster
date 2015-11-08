1588f9d9f42a41fc95369582f75574c97f514217
ildefra <defranceschi.java@gmail.com>
2015-11-08 23:51:15 +0100

    #41 removed show view from achievements

--- src/main/java/org/m4/bgw/web/AchievementController.java
+++ src/main/java/org/m4/bgw/web/AchievementController.java
@@ -1,7 +1,8 @@
 package org.m4.bgw.web;
-import java.io.UnsupportedEncodingException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+
 import org.m4.bgw.domain.AchievedRepository;
 import org.m4.bgw.domain.Achievement;
 import org.m4.bgw.domain.AchievementRepository;
@@ -14,8 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.util.UriUtils;
-import org.springframework.web.util.WebUtils;
+
 
 @RequestMapping("/achievements")
 @Controller
@@ -31,14 +31,19 @@ public class AchievementController {
     AchievementTranslationRepository achievementTranslationRepository;
 
 	@RequestMapping(method = RequestMethod.POST, produces = "text/html")
-    public String create(@Valid Achievement achievement, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+    public String create(
+            @Valid Achievement achievement,
+            BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
         if (bindingResult.hasErrors()) {
             populateEditForm(uiModel, achievement);
             return "achievements/create";
         }
-        uiModel.asMap().clear();
         achievementRepository.save(achievement);
-        return "redirect:/achievements/" + encodeUrlPathSegment(achievement.getAchievementId().toString(), httpServletRequest);
+        
+        uiModel.asMap().clear();
+        uiModel.addAttribute("page", "1");
+        uiModel.addAttribute("size", "10");
+        return "redirect:/achievements";
     }
 
 	@RequestMapping(params = "form", produces = "text/html")
@@ -47,15 +52,14 @@ public class AchievementController {
         return "achievements/create";
     }
 
-	@RequestMapping(value = "/{achievementId}", produces = "text/html")
-    public String show(@PathVariable("achievementId") Short achievementId, Model uiModel) {
-        uiModel.addAttribute("achievement", achievementRepository.findOne(achievementId));
-        uiModel.addAttribute("itemId", achievementId);
-        return "achievements/show";
-    }
 
 	@RequestMapping(produces = "text/html")
-    public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
+    public String list(
+            @RequestParam(value = "page", required = false) Integer page,
+            @RequestParam(value = "size", required = false) Integer size,
+            @RequestParam(value = "sortFieldName", required = false) String sortFieldName,
+            @RequestParam(value = "sortOrder", required = false) String sortOrder,
+            Model uiModel) {
         if (page != null || size != null) {
             int sizeNo = size == null ? 10 : size.intValue();
             final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
@@ -69,14 +73,19 @@ public class AchievementController {
     }
 
 	@RequestMapping(method = RequestMethod.PUT, produces = "text/html")
-    public String update(@Valid Achievement achievement, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+    public String update(
+            @Valid Achievement achievement,
+            BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
         if (bindingResult.hasErrors()) {
             populateEditForm(uiModel, achievement);
             return "achievements/update";
         }
-        uiModel.asMap().clear();
         achievementRepository.save(achievement);
-        return "redirect:/achievements/" + encodeUrlPathSegment(achievement.getAchievementId().toString(), httpServletRequest);
+        
+        uiModel.asMap().clear();
+        uiModel.addAttribute("page", "1");
+        uiModel.addAttribute("size", "10");
+        return "redirect:/achievements";
     }
 
 	@RequestMapping(value = "/{achievementId}", params = "form", produces = "text/html")
@@ -86,9 +95,13 @@ public class AchievementController {
     }
 
 	@RequestMapping(value = "/{achievementId}", method = RequestMethod.DELETE, produces = "text/html")
-    public String delete(@PathVariable("achievementId") Short achievementId, @RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, Model uiModel) {
+    public String delete(
+            @PathVariable("achievementId") Short achievementId,
+            @RequestParam(value = "page", required = false) Integer page,
+            @RequestParam(value = "size", required = false) Integer size, Model uiModel) {
         Achievement achievement = achievementRepository.findOne(achievementId);
         achievementRepository.delete(achievement);
+        
         uiModel.asMap().clear();
         uiModel.addAttribute("page", (page == null) ? "1" : page.toString());
         uiModel.addAttribute("size", (size == null) ? "10" : size.toString());
@@ -99,16 +112,5 @@ public class AchievementController {
         uiModel.addAttribute("achievement", achievement);
         uiModel.addAttribute("achieveds", achievedRepository.findAll());
         uiModel.addAttribute("achievementtranslations", achievementTranslationRepository.findAll());
-    }
-
-	String encodeUrlPathSegment(String pathSegment, HttpServletRequest httpServletRequest) {
-        String enc = httpServletRequest.getCharacterEncoding();
-        if (enc == null) {
-            enc = WebUtils.DEFAULT_CHARACTER_ENCODING;
-        }
-        try {
-            pathSegment = UriUtils.encodePathSegment(pathSegment, enc);
-        } catch (UnsupportedEncodingException uee) {}
-        return pathSegment;
     }
 }
