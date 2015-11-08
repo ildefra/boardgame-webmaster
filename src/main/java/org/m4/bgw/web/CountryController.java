46851e420b9f05e009091ff1c014ba60535996ad
ildefra <defranceschi.java@gmail.com>
2015-11-08 21:16:28 +0100

    #31 removed show view from countries and languages

--- src/main/java/org/m4/bgw/web/CountryController.java
+++ src/main/java/org/m4/bgw/web/CountryController.java
@@ -1,12 +1,12 @@
 package org.m4.bgw.web;
-import org.m4.bgw.domain.Country;
+
 import org.m4.bgw.domain.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RequestMapping("/countrys")
 @Controller
@@ -15,15 +15,13 @@ public class CountryController {
 	@Autowired
     CountryRepository countryRepository;
 
-	@RequestMapping(value = "/{isoAlpha2}", produces = "text/html")
-    public String show(@PathVariable("isoAlpha2") String isoAlpha2, Model uiModel) {
-        uiModel.addAttribute("country", countryRepository.findOne(isoAlpha2));
-        uiModel.addAttribute("itemId", isoAlpha2);
-        return "countrys/show";
-    }
-
 	@RequestMapping(produces = "text/html")
-    public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
+    public String list(
+            @RequestParam(value = "page", required = false) Integer page,
+            @RequestParam(value = "size", required = false) Integer size,
+            @RequestParam(value = "sortFieldName", required = false) String sortFieldName,
+            @RequestParam(value = "sortOrder", required = false) String sortOrder,
+            Model uiModel) {
         if (page != null || size != null) {
             int sizeNo = size == null ? 10 : size.intValue();
             final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
--- src/main/java/org/m4/bgw/web/LanguageController.java
+++ src/main/java/org/m4/bgw/web/LanguageController.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,15 +17,13 @@ public class LanguageController {
 	@Autowired
     LanguageRepository languageRepository;
 
-	@RequestMapping(value = "/{isoCode}", produces = "text/html")
-    public String show(@PathVariable("isoCode") String isoCode, Model uiModel) {
-        uiModel.addAttribute("language", languageRepository.findOne(isoCode));
-        uiModel.addAttribute("itemId", isoCode);
-        return "languages/show";
-    }
-
 	@RequestMapping(produces = "text/html")
-    public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
+    public String list(
+            @RequestParam(value = "page", required = false) Integer page,
+            @RequestParam(value = "size", required = false) Integer size,
+            @RequestParam(value = "sortFieldName", required = false) String sortFieldName,
+            @RequestParam(value = "sortOrder", required = false) String sortOrder,
+            Model uiModel) {
         if (page != null || size != null) {
             int sizeNo = size == null ? 10 : size.intValue();
             final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
