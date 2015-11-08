fd66e84822f7b4c948cc98d7c59b319c46679be2
ildefra <defranceschi.java@gmail.com>
2015-11-08 21:38:52 +0100

    #31 removed show view from time limits

--- src/main/java/org/m4/bgw/web/TimeLimitController.java
+++ src/main/java/org/m4/bgw/web/TimeLimitController.java
@@ -1,7 +1,8 @@
 package org.m4.bgw.web;
-import java.io.UnsupportedEncodingException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+
 import org.m4.bgw.domain.AvgGameLengthRepository;
 import org.m4.bgw.domain.GameTableRepository;
 import org.m4.bgw.domain.TimeLimit;
@@ -14,8 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.util.UriUtils;
-import org.springframework.web.util.WebUtils;
+
 
 @RequestMapping("/timelimits")
 @Controller
@@ -31,14 +31,19 @@ public class TimeLimitController {
     GameTableRepository gameTableRepository;
 
 	@RequestMapping(method = RequestMethod.POST, produces = "text/html")
-    public String create(@Valid TimeLimit timeLimit, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+    public String create(
+            @Valid TimeLimit timeLimit,
+            BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
         if (bindingResult.hasErrors()) {
             populateEditForm(uiModel, timeLimit);
             return "timelimits/create";
         }
-        uiModel.asMap().clear();
         timeLimitRepository.save(timeLimit);
-        return "redirect:/timelimits/" + encodeUrlPathSegment(timeLimit.getTimeLimitId().toString(), httpServletRequest);
+        
+        uiModel.asMap().clear();
+        uiModel.addAttribute("page", "1");
+        uiModel.addAttribute("size", "10");
+        return "redirect:/timelimits";
     }
 
 	@RequestMapping(params = "form", produces = "text/html")
@@ -47,15 +52,14 @@ public class TimeLimitController {
         return "timelimits/create";
     }
 
-	@RequestMapping(value = "/{timeLimitId}", produces = "text/html")
-    public String show(@PathVariable("timeLimitId") Integer timeLimitId, Model uiModel) {
-        uiModel.addAttribute("timelimit", timeLimitRepository.findOne(timeLimitId));
-        uiModel.addAttribute("itemId", timeLimitId);
-        return "timelimits/show";
-    }
 
 	@RequestMapping(produces = "text/html")
-    public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
+    public String list(
+            @RequestParam(value = "page", required = false) Integer page,
+            @RequestParam(value = "size", required = false) Integer size,
+            @RequestParam(value = "sortFieldName", required = false) String sortFieldName,
+            @RequestParam(value = "sortOrder", required = false) String sortOrder,
+            Model uiModel) {
         if (page != null || size != null) {
             int sizeNo = size == null ? 10 : size.intValue();
             final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
@@ -69,14 +73,19 @@ public class TimeLimitController {
     }
 
 	@RequestMapping(method = RequestMethod.PUT, produces = "text/html")
-    public String update(@Valid TimeLimit timeLimit, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+    public String update(
+            @Valid TimeLimit timeLimit,
+            BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
         if (bindingResult.hasErrors()) {
             populateEditForm(uiModel, timeLimit);
             return "timelimits/update";
         }
-        uiModel.asMap().clear();
         timeLimitRepository.save(timeLimit);
-        return "redirect:/timelimits/" + encodeUrlPathSegment(timeLimit.getTimeLimitId().toString(), httpServletRequest);
+        
+        uiModel.asMap().clear();
+        uiModel.addAttribute("page", "1");
+        uiModel.addAttribute("size", "10");
+        return "redirect:/timelimits";
     }
 
 	@RequestMapping(value = "/{timeLimitId}", params = "form", produces = "text/html")
@@ -86,7 +95,11 @@ public class TimeLimitController {
     }
 
 	@RequestMapping(value = "/{timeLimitId}", method = RequestMethod.DELETE, produces = "text/html")
-    public String delete(@PathVariable("timeLimitId") Integer timeLimitId, @RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, Model uiModel) {
+    public String delete(
+            @PathVariable("timeLimitId") Integer timeLimitId,
+            @RequestParam(value = "page", required = false) Integer page,
+            @RequestParam(value = "size", required = false) Integer size,
+            Model uiModel) {
         TimeLimit timeLimit = timeLimitRepository.findOne(timeLimitId);
         timeLimitRepository.delete(timeLimit);
         uiModel.asMap().clear();
@@ -99,16 +112,5 @@ public class TimeLimitController {
         uiModel.addAttribute("timeLimit", timeLimit);
         uiModel.addAttribute("avggamelengths", avgGameLengthRepository.findAll());
         uiModel.addAttribute("gametables", gameTableRepository.findAll());
-    }
-
-	String encodeUrlPathSegment(String pathSegment, HttpServletRequest httpServletRequest) {
-        String enc = httpServletRequest.getCharacterEncoding();
-        if (enc == null) {
-            enc = WebUtils.DEFAULT_CHARACTER_ENCODING;
-        }
-        try {
-            pathSegment = UriUtils.encodePathSegment(pathSegment, enc);
-        } catch (UnsupportedEncodingException uee) {}
-        return pathSegment;
     }
 }
