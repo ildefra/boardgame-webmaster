41d98b9726f86c0ba7d69849f1df8eeb631b9c5b
ildefra <defranceschi.java@gmail.com>
2015-11-09 16:16:16 +0100

    #54 achievement translations

--- src/main/java/org/m4/bgw/web/AchievementTranslationController.java
+++ src/main/java/org/m4/bgw/web/AchievementTranslationController.java
@@ -1,7 +1,8 @@
 package org.m4.bgw.web;
-import java.io.UnsupportedEncodingException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+
 import org.m4.bgw.domain.AchievementRepository;
 import org.m4.bgw.domain.AchievementTranslation;
 import org.m4.bgw.domain.AchievementTranslationPK;
@@ -9,6 +10,7 @@ import org.m4.bgw.domain.AchievementTranslationRepository;
 import org.m4.bgw.domain.LanguageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,8 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.util.UriUtils;
-import org.springframework.web.util.WebUtils;
+
 
 @RequestMapping("/achievementtranslations")
 @Controller
@@ -41,14 +42,19 @@ public class AchievementTranslationController {
     }
 
 	@RequestMapping(method = RequestMethod.POST, produces = "text/html")
-    public String create(@Valid AchievementTranslation achievementTranslation, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+    public String create(
+            @Valid AchievementTranslation achievementTranslation,
+            BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
         if (bindingResult.hasErrors()) {
             populateEditForm(uiModel, achievementTranslation);
             return "achievementtranslations/create";
         }
-        uiModel.asMap().clear();
         achievementTranslationRepository.save(achievementTranslation);
-        return "redirect:/achievementtranslations/" + encodeUrlPathSegment(conversionService.convert(achievementTranslation.getId(), String.class), httpServletRequest);
+        
+        uiModel.asMap().clear();
+        uiModel.addAttribute("page", "1");
+        uiModel.addAttribute("size", "10");
+        return "redirect:/achievementtranslations";
     }
 
 	@RequestMapping(params = "form", produces = "text/html")
@@ -65,11 +71,18 @@ public class AchievementTranslationController {
     }
 
 	@RequestMapping(produces = "text/html")
-    public String list(@RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, @RequestParam(value = "sortFieldName", required = false) String sortFieldName, @RequestParam(value = "sortOrder", required = false) String sortOrder, Model uiModel) {
+    public String list(
+            @RequestParam(value = "page", required = false) Integer page,
+            @RequestParam(value = "size", required = false) Integer size,
+            @RequestParam(value = "sortFieldName", required = false) String sortFieldName,
+            @RequestParam(value = "sortOrder", required = false) String sortOrder,
+            Model uiModel) {
         if (page != null || size != null) {
             int sizeNo = size == null ? 10 : size.intValue();
             final int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
-            uiModel.addAttribute("achievementtranslations", achievementTranslationRepository.findAll(new org.springframework.data.domain.PageRequest(firstResult / sizeNo, sizeNo)).getContent());
+            uiModel.addAttribute(
+                    "achievementtranslations",
+                    achievementTranslationRepository.findAll(new PageRequest(firstResult / sizeNo, sizeNo)).getContent());
             float nrOfPages = (float) achievementTranslationRepository.count() / sizeNo;
             uiModel.addAttribute("maxPages", (int) ((nrOfPages > (int) nrOfPages || nrOfPages == 0.0) ? nrOfPages + 1 : nrOfPages));
         } else {
@@ -79,14 +92,19 @@ public class AchievementTranslationController {
     }
 
 	@RequestMapping(method = RequestMethod.PUT, produces = "text/html")
-    public String update(@Valid AchievementTranslation achievementTranslation, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
+    public String update(
+            @Valid AchievementTranslation achievementTranslation,
+            BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) {
         if (bindingResult.hasErrors()) {
             populateEditForm(uiModel, achievementTranslation);
             return "achievementtranslations/update";
         }
-        uiModel.asMap().clear();
         achievementTranslationRepository.save(achievementTranslation);
-        return "redirect:/achievementtranslations/" + encodeUrlPathSegment(conversionService.convert(achievementTranslation.getId(), String.class), httpServletRequest);
+        
+        uiModel.asMap().clear();
+        uiModel.addAttribute("page", "1");
+        uiModel.addAttribute("size", "10");
+        return "redirect:/achievementtranslations";
     }
 
 	@RequestMapping(value = "/{id}", params = "form", produces = "text/html")
@@ -95,8 +113,13 @@ public class AchievementTranslationController {
         return "achievementtranslations/update";
     }
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = "text/html")
-    public String delete(@PathVariable("id") AchievementTranslationPK id, @RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size, Model uiModel) {
+	@RequestMapping(
+	        value = "/{id}", method = RequestMethod.DELETE, produces = "text/html")
+    public String delete(
+            @PathVariable("id") AchievementTranslationPK id,
+            @RequestParam(value = "page", required = false) Integer page,
+            @RequestParam(value = "size", required = false) Integer size,
+            Model uiModel) {
         AchievementTranslation achievementTranslation = achievementTranslationRepository.findOne(id);
         achievementTranslationRepository.delete(achievementTranslation);
         uiModel.asMap().clear();
@@ -109,16 +132,5 @@ public class AchievementTranslationController {
         uiModel.addAttribute("achievementTranslation", achievementTranslation);
         uiModel.addAttribute("achievements", achievementRepository.findAll());
         uiModel.addAttribute("languages", languageRepository.findAll());
-    }
-
-	String encodeUrlPathSegment(String pathSegment, HttpServletRequest httpServletRequest) {
-        String enc = httpServletRequest.getCharacterEncoding();
-        if (enc == null) {
-            enc = WebUtils.DEFAULT_CHARACTER_ENCODING;
-        }
-        try {
-            pathSegment = UriUtils.encodePathSegment(pathSegment, enc);
-        } catch (UnsupportedEncodingException uee) {}
-        return pathSegment;
     }
 }
